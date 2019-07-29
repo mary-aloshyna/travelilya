61f74f43be1ea5843498a8fb8d66c4df2de10255
Mary Aloshyna <46464374+mary-aloshyna@users.noreply.github.com>
2019-07-29 20:12:22 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,20 +1,12 @@
 $(document).ready(function () {
     $('.gallery__row--mobile').slick({
         centerMode: true,
+        arrows: false,
         centerPadding: '60px',
-        slidesToShow: 3,
+        slidesToShow: 1,
         responsive: [
             {
                 breakpoint: 768,
-                settings: {
-                    arrows: false,
-                    centerMode: true,
-                    centerPadding: '40px',
-                    slidesToShow: 3
-                }
-            },
-            {
-                breakpoint: 480,
                 settings: {
                     arrows: false,
                     centerMode: true,
